038f0d03375edde709fd8302051f09062df74f56
karinbez <karin.bez@gmail.com>
2016-05-23 12:59:49 -0300

    comit do projeto twitter

---
+++ ProjetoTwitter/src/EncontrarElementos.java
@@ -0,0 +1,38 @@
+import org.junit.Test;
+import java.util.List;
+import org.openqa.selenium.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+	public class EncontrarElementos {
+		
+		public static void main(String[] args) {
+			
+			WebDriver driver = new FirefoxDriver();
+			driver.get("https://twitter.com/login/");
+			driver.manage().window().maximize();
+			driver.findElement(By.xpath("//div[@class='signin-wrapper']//input[@name='session[username_or_email]']")).sendKeys("karin_bez@hotmail.com");
+			driver.findElement(By.xpath("//div[@class='signin-wrapper']//input[@name='session[password]']")).sendKeys("kaka5482");
+			driver.findElement(By.xpath("//div[@class='signin-wrapper']//button[@class='submit btn primary-btn']")).click();
+			driver.findElement(By.id("tweet-box-home-timeline")).sendKeys("teste legal do Twitter");
+			driver.findElement(By.xpath("//div[@class='TweetBoxToolbar']//button[@class='btn primary-btn tweet-action tweet-btn js-tweet-btn']")).click();
+			
+		}
+		
+		public static WebElement getVisibleElement(WebDriver driver, String name) {
+		    List<WebElement> elements = driver.findElements(By.name(name));
+		    for (WebElement element : elements) {
+		        if (element.isDisplayed()) {
+		            return element;
+		        }
+		    }
+		    return null;
+		
+		}
+	}
+
+	
+
+
